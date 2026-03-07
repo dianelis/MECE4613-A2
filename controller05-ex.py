fd0350ca3a3e23ab3a2c3eab1e6067aae60ad92c
dianelis <dianelisinsua@gmail.com>
2026-03-07 18:40:46 -0500

    Implement touch-controlled LED colors

--- controller05-ex.py
+++ controller05-ex.py
@@ -10,14 +10,29 @@ This is how this script should work:
 
 # import required libraries
 from adafruit_crickit import crickit
+import time
 
 
-
+# define hyper parameters
+RGB = dict(red=0xFF0000, green=0x00FF00, blue=0x0000FF)
+crickit.onboard_pixel.brightness = 0.01
 
 
 def main():
     # YOUR CODE STARTS HERE
-    ?
+    print("Touch 1=blue, 2=green, 3=red, 4=exit")
+
+    while True:
+        if crickit.touch_1.value:
+            crickit.onboard_pixel.fill(RGB["blue"])
+        elif crickit.touch_2.value:
+            crickit.onboard_pixel.fill(RGB["green"])
+        elif crickit.touch_3.value:
+            crickit.onboard_pixel.fill(RGB["red"])
+        elif crickit.touch_4.value:
+            break
+
+        time.sleep(0.05)
     # YOUR CODE ENDS HERE
 
 
