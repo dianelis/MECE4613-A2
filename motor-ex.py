3fa7e57e7543a8388b174d961914f029d64a2deb
Diane Insua <ddi4@cornell.edu>
2026-03-07 19:45:37 -0500

    Implement motor-ex.py: sync coefficients, set_throttle, and move function

--- motor-ex.py
+++ motor-ex.py
@@ -20,8 +20,8 @@ It's a very powerful tool to create a customized function with pre-defined or ne
 
 # Hyper parameters of your robot that you found in controller09-ex.py exercise.
 # NOTE: each robot is different; if you haven't found it yet, go back to that exercise and find these coefficients.
-SyncForwardR = ?    # the right motor's sync coefficient when it moves forward
-SyncBackwardR = ?   # the right motor's sync coefficient when it moves backward
+SyncForwardR = 0.95    # the right motor's sync coefficient when it moves forward
+SyncBackwardR = 0.95   # the right motor's sync coefficient when it moves backward
 # We now define two standard dictionaries which the script will refer it:
 MOTOR = {'R': ck.dc_motor_1, 'L': ck.dc_motor_2}            # dictionary of motors
 THROTTLE_SPEED = {0: 0, 1: 0.35, 2: 0.5, 3: 0.7, 4: 0.9}    # dictionary of speeds; 0: stop, 4: max speed
@@ -41,7 +41,10 @@ def set_throttle(motor_name, speed, factor=1):
         - applies current to the dc motors wrt the SyncForwardR or SyncBackwardR hyper params
     """
     # If the motor_name is 'R' then, we have to apply sync coefficients - depending on the direction:
-    ?
+    if motor_name == 'R':
+        sync = SyncForwardR if factor >= 0 else SyncBackwardR
+    else:
+        sync = 1
     # Now, we set the throttle speed to the given motor:
     MOTOR[motor_name].throttle = THROTTLE_SPEED[speed] * sync * factor
  
@@ -59,13 +62,14 @@ def move(duration=0.3, speed=2, factor_r=1, factor_l=1):
         - the motors' motion
     """
     # set the throttle speed to the right motor first
-    ?
+    set_throttle('R', speed, factor_r)
     # set the throttle speed to the left motor
-    ?
+    set_throttle('L', speed, factor_l)
     # set the sleep time = duration
-    ?
+    time.sleep(duration)
     # turn off both motors by setting up the throttles to 0
-    ?
+    set_throttle('R', 0)
+    set_throttle('L', 0)
  
 
 # Now we leverage power of 'partial' to redefine move() function and create new functions out of it.
